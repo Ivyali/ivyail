ba035c1141acc21acd6a619e9e82d0fd35a6f565
Michael Stanchina <Mikey@Kellys-MacBook-Air.local>
2026-03-06 10:07:00 -0500

    fix cancel route

---
+++ app/cancel/page.tsx
@@ -0,0 +1,9 @@
+export default function CancelPage() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Checkout Cancelled
+      </h1>
+    </div>
+  );
+}
